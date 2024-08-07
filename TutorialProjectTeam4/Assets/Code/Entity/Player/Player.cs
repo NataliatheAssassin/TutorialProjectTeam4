47ef2b334ae98ffd860ffca62c01cf5dfc3c623e
NataliatheAssassin <eigensoyul@gmail.com>
2024-08-07 14:27:12 +0900

    fixed bug

--- TutorialProjectTeam4/Assets/Code/Entity/Player/Player.cs
+++ TutorialProjectTeam4/Assets/Code/Entity/Player/Player.cs
@@ -8,7 +8,7 @@ public class Player : Entity
     private Rigidbody2D rb { get => GetComponent<Rigidbody2D>(); }
 
     private Vector2 accDirection;
-    private float speed = 150;
+    private float speed;
     private float magazine; //magazine == 탄창... 전 몰랐어요 ㅠㅠ
     public float GetMagazine() { return magazine; }
     public void SetMagazine(float value) { if (value <= 50) magazine = value; else { magazine = 50; } }
@@ -19,6 +19,7 @@ public class Player : Entity
     {
         maxHp = 10f;
 
+        speed = 150;
         magazine = 50;
     }
 
@@ -63,7 +64,7 @@ public class Player : Entity
     {
         hp = 0; //혹시 나중에 hp 바를 추가할 때를 대비해서, 추가함. 음수로 간 hp 바를 렌더링해봤자 이쁠 게 없음.
         Instantiate(deathEffect, transform.position, transform.rotation);
-        SceneManager.LoadScene("Main_Menu");
+        SceneManager.LoadScene("In_Game");
         Destroy(gameObject);
     }
 
--- TutorialProjectTeam4/Assets/Code/GameSystem/StageSystem.cs
+++ TutorialProjectTeam4/Assets/Code/GameSystem/StageSystem.cs
@@ -12,21 +12,22 @@ public class StageSystem : MonoBehaviour
 
     [SerializeField] GameObject[] enemy, box, item;
 
-    private void OnEnable()
+    private void Start()
     {
+        stageNumber = 1;
         SetStage();
     }
 
     public void SetStage()
     {
         //여기 밑에 있는 것들은 일단 테스트용... ㅎㅎ
-        if (stageNumber == 1)
+        if (stageNumber == 1) //tutorail 1
         {
             Instantiate(box[0], new Vector2(0f, 0f), transform.rotation);
             Instantiate(item[0], new Vector2(12f, -4.5f), transform.rotation);
             Instantiate(item[1], new Vector2(12f, 4.5f), transform.rotation);
         }
-        else if (stageNumber == 2)
+        else if (stageNumber == 2) //tutorial 2
         {
             Instantiate(box[0], new Vector2(23f, -7f), transform.rotation);
 
@@ -37,11 +38,10 @@ public class StageSystem : MonoBehaviour
             Instantiate(enemy[0], new Vector2(40f, -4f), transform.rotation);
             Instantiate(enemy[3], new Vector2(46f, -4f), transform.rotation);
         }
-        else if (stageNumber == 3)
+        else if (stageNumber == 3) //stage 1
         {
-
         }
-        else if (stageNumber == 4)
+        else if (stageNumber == 4) //stage 2
         {
 
         }
